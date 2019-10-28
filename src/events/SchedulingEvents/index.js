bea085c0094853bc93af1f50557a0c95bdc14364
lukassxp <lpalucas.10@gmail.com>
2019-10-28 18:59:38 -0300

    fixed variable names

--- src/events/SchedulingEvents/index.js
+++ src/events/SchedulingEvents/index.js
@@ -1,14 +1,14 @@
 import { CompressionTypes } from 'kafkajs';
 
-export default class AquariumEvents {
+export default class SchedulingEvents {
   constructor(server) {
-    this.aquariumSocket = server.of('/scheduling');
+    this.schedulingSocket = server.of('/scheduling');
 
     this.initEvents();
   }
 
   initEvents() {
-    this.aquariumSocket.on('connection', client => {
+    this.schedulingSocket.on('connection', client => {
       client.on('CLIENT_INFO', async message => {
         console.log(
           `Scheduling client connected - Name: ${message.params.name}`
